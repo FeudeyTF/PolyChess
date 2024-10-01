435ad6a359395214b940cff44c41f8db97dfeb59
FeudeyTF <yanmordanenko089@gmail.com>
2024-10-01 23:26:24 +0300

    Поменял кавычки на нормальные

--- PolyChessTGBot/Bot/Commands/Registrator.cs
+++ PolyChessTGBot/Bot/Commands/Registrator.cs
@@ -56,7 +56,7 @@ namespace PolyChessTGBot.Bot.Commands
             foreach (var command in Commands)
                 if (command.Names.Contains(commandName))
                 {
-                    Program.Logger.Write($"Получена команда: `{message.Text}`. Аргументы: {string.Join(", ", args.Parameters)}", LogType.Info);
+                    Program.Logger.Write($"Получена команда: '{message.Text}'. Аргументы: {string.Join(", ", args.Parameters)}", LogType.Info);
                     try
                     {
                         await command.Delegate(args);
