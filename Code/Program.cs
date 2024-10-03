88c3f42ce0696089dd5d56bab3fee62b385c84c2
FeudeyTF <yanmordanenko089@gmail.com>
2024-10-04 01:35:30 +0300

    Теперь код хранится в папке Code, а иконки - Icons

--- Code/Program.cs
+++ Code/Program.cs
@@ -8,7 +8,7 @@ namespace PolyChessTGBot
 {
     public static class Program
     {
-        public readonly static Version Version = new(0, 1, 18);
+        public readonly static Version Version = new(0, 2, 0);
 
         public readonly static DateTime Started;
 
