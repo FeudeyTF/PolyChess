fb63983f9156cb53d54e87f418ab04aaea1d11b8
FeudeyTF <yanmordanenko089@gmail.com>
2024-10-06 20:56:18 +0300

    Фикс даты

--- Code/Bot/Commands.cs
+++ Code/Bot/Commands.cs
@@ -75,7 +75,7 @@ namespace PolyChessTGBot.Bot
                 ];
                 var data = TelegramButtonData.GetDataString("QuestionDataID", ("ID", args.User.Id), ("ChannelID", args.Message.MessageId));
                 InlineKeyboardMarkup uesrInfo = new(new InlineKeyboardButton("Данные") { CallbackData = data });
-                await args.Bot.SendTextMessageAsync(Program.MainConfig.QuestionChannel, string.Join("\n", message).RemoveBadSymbols(), parseMode: ParseMode.Html, replyMarkup: uesrInfo);
+                await args.Bot.SendTextMessageAsync(Program.MainConfig.QuestionChannel, string.Join("\n", message), parseMode: ParseMode.Html, replyMarkup: uesrInfo);
                 await args.Reply("Ваш вопрос был успешно отправлен!");
             }
             else
