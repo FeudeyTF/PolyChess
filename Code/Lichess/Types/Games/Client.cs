6572db363f533acbc5fad886db1f2a7595db3d78
FeudeyTF <yanmordanenko089@gmail.com>
2024-10-13 16:11:41 +0300

    Фикс

--- Code/Lichess/Types/Games/Client.cs
+++ Code/Lichess/Types/Games/Client.cs
@@ -1,16 +1,6 @@
-﻿using PolyChessTGBot.Lichess.Types;
-
-namespace PolyChessTGBot.Lichess
+﻿namespace PolyChessTGBot.Lichess
 {
     public partial class LichessApiClient
     {
-
-        public async Task<List<Team>> GetUserTeamsAsync(string username)
-        {
-            var teams = await GetJsonObject<List<Team>>("team", "of", username);
-            if (teams == null)
-                return [];
-            return teams;
-        }
     }
 }
