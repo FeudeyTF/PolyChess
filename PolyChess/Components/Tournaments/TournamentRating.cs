68accc2e60f45433cc790946e752eafb1d2b8fed
FeudeyTF <yanmordanenko089@gmail.com>
2025-10-30 23:59:10 +0300

    Заменил конструктор

--- PolyChess/Components/Tournaments/TournamentRating.cs
+++ PolyChess/Components/Tournaments/TournamentRating.cs
@@ -1,9 +1,15 @@
 ﻿namespace PolyChess.Components.Tournaments
 {
-    internal struct TournamentRating<TValue>(Dictionary<DivisionType, List<TValue>> divisions, List<TournamentUser<TValue>> players)
+    internal struct TournamentRating<TValue>
     {
-        public Dictionary<DivisionType, List<TValue>> Divisions { get; set; } = divisions;
+        public Dictionary<DivisionType, List<TValue>> Divisions { get; set; }
 
-        public List<TournamentUser<TValue>> Players { get; set; } = players;
+        public List<TournamentUser<TValue>> Players { get; set; }
+
+        public TournamentRating(Dictionary<DivisionType, List<TValue>> divisions, List<TournamentUser<TValue>> players)
+        {
+            Divisions = divisions;
+            Players = players;
+        }
     }
 }
