e074cb4a09ef6c23ff5d80befdee051b1f1192c5
FeudeyTF <yanmordanenko089@gmail.com>
2025-11-20 12:48:17 +0300

    Добавил цвет в вывод логера

--- PolyChess/Core/Logging/Types/ConsoleLogger.cs
+++ PolyChess/Core/Logging/Types/ConsoleLogger.cs
@@ -4,7 +4,18 @@
     {
         public void Write(string message, LogLevel logLevel)
         {
+            ConsoleColor color = logLevel switch 
+            { 
+                LogLevel.Debug => ConsoleColor.Yellow,
+                LogLevel.Error => ConsoleColor.Red,
+                LogLevel.Warn => ConsoleColor.Red | ConsoleColor.Yellow,
+                LogLevel.Info => ConsoleColor.Green,
+                _ => ConsoleColor.White
+                
+            };
+            Console.ForegroundColor = color;
             Console.WriteLine(logLevel.ToString().ToUpper() + ": " + message);
+            Console.ResetColor();
         }
     }
 }
