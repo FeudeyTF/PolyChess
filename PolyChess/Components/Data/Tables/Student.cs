612ea5fa4a0ca7b846a65fdfea7bb5f4fe0188de
FeudeyTF <yanmordanenko089@gmail.com>
2026-01-16 03:29:04 +0300

    Изменил ключ для значений таблицы Students

--- PolyChess/Components/Data/Tables/Student.cs
+++ PolyChess/Components/Data/Tables/Student.cs
@@ -2,36 +2,37 @@
 
 namespace PolyChess.Components.Data.Tables
 {
-    [PrimaryKey(nameof(Name), nameof(Surname), nameof(Patronymic))]
-    internal class Student
-    {
-        public string Name { get; set; } = null!;
+	internal class Student
+	{
+		public int Id { get; set; }
 
-        public string Surname { get; set; } = null!;
+		public string Name { get; set; } = null!;
 
-        public string Patronymic { get; set; } = null!;
+		public string Surname { get; set; } = null!;
 
-        public string? LichessId { get; set; } = null;
+		public string Patronymic { get; set; } = null!;
 
-        public long TelegramId { get; set; }
+		public string? LichessId { get; set; } = null;
 
-        public long Year { get; set; }
+		public long TelegramId { get; set; }
 
-        public string Institute { get; set; } = null!;
+		public long Year { get; set; }
 
-        public string Group { get; set; } = null!;
+		public string Institute { get; set; } = null!;
 
-        public string? RecordBookId { get; set; }
+		public string Group { get; set; } = null!;
 
-        public bool CreativeTaskCompleted { get; set; }
+		public string? RecordBookId { get; set; }
 
-        public string? LichessToken { get; set; }
+		public bool CreativeTaskCompleted { get; set; }
 
-        public int AdditionalTournamentsScore { get; set; }
+		public string? LichessToken { get; set; }
 
-        public override string ToString()
-        {
-            return $"Имя: <b>{Name}</b>, Lichess: <b>{LichessId}</b>, Telegram: <b>{TelegramId}</b>, Курс: <b>{Year}</b>";
-        }
-    }
+		public int AdditionalTournamentsScore { get; set; }
+
+		public override string ToString()
+		{
+			return $"Имя: <b>{Name}</b>, Lichess: <b>{LichessId}</b>, Telegram: <b>{TelegramId}</b>, Курс: <b>{Year}</b>";
+		}
+	}
 }
